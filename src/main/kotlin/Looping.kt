8255954daa03dfc4315fed3e740bd8c949b5b830
mounirrouissi <you@example.com>
2021-06-29 21:57:45 +0200

    looping

---
+++ src/main/kotlin/Looping.kt
@@ -0,0 +1,46 @@
+fun main() {
+
+    println("***while***")
+    var x=0;
+    while (x<5){
+
+        println(x)
+x++
+    }
+
+
+    println("***for***")
+
+    for (x in 1..10 ) {
+        println(x)
+    }
+
+
+    // .. == until
+
+    println("***for until***")
+
+    for (x in 1 until 10 ) {
+        println(x)
+    }
+
+    println("***for step***")
+
+    for (x in 1..10 step 5) {
+        println(x)
+    }
+
+
+    println("***for dwonTo***")
+
+    for (x in 9 downTo 1) {
+        println(x)
+    }
+
+
+
+
+    }
+
+
+
